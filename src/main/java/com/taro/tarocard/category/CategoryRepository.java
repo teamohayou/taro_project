fba0c6ecaf0ca6cbf87ab554579c23b3a1000083
yoonha-yu <yoonfoy54@gmail.com>
2024-10-02 13:14:42 +0900

    Revert "기존 만들어놓은거"

--- src/main/java/com/taro/tarocard/category/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taro.tarocard.category;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
